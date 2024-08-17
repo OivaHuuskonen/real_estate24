6862dc12465150f4cd7e4231cc9a91ebac62f77a
OivaHuuskonen <Solarsystems3@gmail.com>
2024-08-17 14:34:31 +0300

    serve

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
 
 app.use(cors({
-  origin: ['http://localhost:5173', '']
+  origin: ['http://localhost:5173', 'https://real-estate0824.onrender.com']
 }));
 
 
