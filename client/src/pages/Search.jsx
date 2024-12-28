5e635f851637f0807966bd895ede528060347dfc
OivaHuuskonen <Solarsystems3@gmail.com>
2024-12-28 23:45:08 +0200

    build_to_destroy

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -22,12 +22,6 @@ export default function Search() {
           )}
         </div>
 
-        {/*<div className="row">
-          {search.results?.map((item) => (
-            <AdCard ad={item} key={item._id} />
-          ))}
-        </div>*/}
-
         <div className="grid grid-cols-1 md:grid-cols-3 justify-center gap-0 gap-y-10 place-items-center px-20 py-10 bg-[#F5F5F5]">
           {search.results?.map((item, index) => (
             <AdCard 
