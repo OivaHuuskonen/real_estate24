86141a10f5a471c9144e2ab4f9016b9e684344fd
OivaHuuskonen <156306030+OivaHuuskonen@users.noreply.github.com>
2025-01-02 20:10:23 +0200

    build_0201

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,24 +2,9 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../context/auth";
 import axios from "axios";
 import AdCard from "../components/cards/AdCard";
-//import SearchForm from "../components/forms/SearchForm";
-import SearchFormRB from "../components/forms/SearchFormRB";
+import SearchMain from "../components/forms/SearchMain";
 //import leafyIcon from '../assets/leafy.png'; 
 //import artNouveau from '../assets/art-nouveau.png'
-
-/*const PageHeader = ({ title }) => (
-  <div className="mx-auto w-full	text-align:left pb-16 pt-20 bg-[#51829B]">
-    <h1 className="font-floral pl-10 text-9xl sm:text-7xl  text-[#F5F5F5]">
-      {title}
-      /*<img src={leafyIcon} alt="Leafy Icon" className="h-12 w-12 ml-4" />*/
-   
-   /* <img src={artNouveau} alt="Art Nouveau" className="h-[1em] w-auto ml-4" /> </h1>
-  </div>
-);
-
-
-minulla on react / vite / tailwindcss "kiinteistövälitys mern stack" jota koe julkaisen render kautta. Sovelluksessa on ideana se että asiakas voi etsiä google maps / places haun avulla kohteita ympäristöstään. Sovellus toimii suunnilleen, näyttää hyvälle suurilta näytöiltä mutta älypuhelin näytöllä <SerachForm /> nappirivistö vuotaa yli näytön oikeasta reunasta, myöskään "price" napin alasveto valikko ei toimi. Osaatko kertoa miten nämä asiat olisi mahdollista korjata? Koodi on nyt:
-*/
 
 const PageHeader = ({ title }) => (
   <div className="w-full text-align:left pb-12 pt-14 xl:pb-16 xl:pt-20 bg-[#51829B]">
@@ -31,8 +16,6 @@ const PageHeader = ({ title }) => (
     </div>
   </div>
 );
-
-
 export default function Home() {
   const [auth, setAuth] = useAuth();
   const [adsForSell, setAdsForSell] = useState([]);
@@ -56,7 +39,7 @@ export default function Home() {
     <div name='home' className='max-w-screen w-full pb-10'>
      
         <div className="flex justify-center py-10 mx-auto">
-        <SearchFormRB />
+        <SearchMain />
         </div>
         <div name="header" className="w-full">
         <PageHeader title="For Sell"/>
@@ -76,32 +59,6 @@ export default function Home() {
         <div name="header" className="w-full">
         <PageHeader title="For Rent"/>
         </div>
-      {/*
-      head 1 justify-center mb-10 gap-y-10 place-items-center px-20 py-10 bg-[#FFFAFA]">
-      <div className="grid grid-cols-1 
-          sm:grid-cols-1 
-          md:grid-cols-2
-          lg:grid-cols-3 
-          justify-center mb-10 gap-y-10 place-items-center px-20 py-10 bg-[#F5F5F5]">
-          {adsForSell.map((ad, index) => (
-            <AdCard 
-              ad={ad} 
-              key={ad._id} 
-              className={index % 3 === 0 ? 'justify-self-end' :
-                         index % 3 === 1 ? '' :
-                         'justify-self-start'}
-              />*/}
-        {/*<div className="grid grid-cols-1 md:grid-cols-3 justify-center mb-10 gap-y-10 place-items-center px-20 py-10 bg-[#F5F5F5]">
-        {adsForRent.map((ad, index) => (
-            <AdCard 
-              ad={ad} 
-              key={ad._id} 
-              className={index % 3 === 0 ? 'justify-self-end' :
-                         index % 3 === 1 ? '' :
-                         'justify-self-start'}
-              />
-          ))}
-        </div>*/}
    
         <div className="grid grid-cols-1 
         sm:grid-cols-1 
