aaab9d32823c948d1c745e2d2c0fa8ee2b05368d
OivaHuuskonen <156306030+OivaHuuskonen@users.noreply.github.com>
2025-02-13 19:01:02 +0200

    build130225ilta

--- client/src/pages/user/ad/AdCreate.jsx
+++ client/src/pages/user/ad/AdCreate.jsx
@@ -40,7 +40,7 @@ export default function AdCreate() {
         <div className="flex flex-col items-center">
           <button
             onClick={handleSell}
-            className="py-2.5 px-10 me-2 mb-2 text-sm font-medium
+            className="py-2.5 px-10 me-2 mb-2 text-sm font-medium font-yeseva-one-regular
             text-gray-900 focus:outline-none bg-white 
             rounded-lg border border-gray-200 hover:bg-gray-100
             hover:text-[#51829B] focus:z-10 focus:ring-4
@@ -55,7 +55,7 @@ export default function AdCreate() {
             <div className="flex justify-center gap-4">
               <button
                 onClick={() => navigate("/ad/create/sell/House")}
-                className="py-2.5 px-5 me-2 mb-2 text-sm font-medium
+                className="py-2.5 px-5 me-2 mb-2 font-yeseva-one-regular text-sm font-medium
                 text-gray-900 focus:outline-none bg-white 
                 rounded-lg border border-gray-200 hover:bg-gray-100
                 hover:text-blue-700 focus:z-10 focus:ring-4
@@ -68,7 +68,7 @@ export default function AdCreate() {
               </button>
               <button
                 onClick={() => navigate("/ad/create/sell/Land")}
-                className="py-2.5 px-5 me-2 mb-2 text-sm font-medium
+                className="py-2.5 px-5 me-2 mb-2 text-sm font-yeseva-one-regular font-medium
                 text-gray-900 focus:outline-none bg-white 
                 rounded-lg border border-gray-200 hover:bg-gray-100
                 hover:text-blue-700 focus:z-10 focus:ring-4
